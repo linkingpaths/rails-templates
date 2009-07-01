c70bd19c0ede468b05416c5e83117e0c8214a820
Aitor García <aitor.garcia@gmail.com>
2009-07-01 12:50:38 +0200

    Move git stuff to the end of the template

--- git_and_rspec.rb
+++ git_and_rspec.rb
@@ -1,3 +1,32 @@
+# regular plugins 
+plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
+plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
+plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
+plugin 'open_id_authentication', :git => 'git://github.com/rails/open_id_authentication.git'
+plugin 'exception_notifier', :git => 'git://github.com/rails/exception_notification.git'
+
+
+# gems  
+gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+gem 'ruby-openid', :lib => 'openid', :source => 'http://gems.github.com'  
+gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com'       
+rake "gems:install", :sudo => true
+ 
+# gems  
+rakefile "bootstrap.rake", <<CODE
+  namespace :app do
+    task :bootstrap do
+    end
+    
+    task :seed do
+    end
+  end
+CODE
+ 
+generate("authenticated", "user session")
+generate("rspec")
+
+
 # Delete unnecessary files
 run "rm README"
 run "rm public/index.html"
@@ -32,33 +61,3 @@ run "cp config/database.yml config/database.yml.sample"
 git :add => "."
  
 git :commit => "-a -m 'Setting up a new rails app. Copy config/database.yml.sample to config/database.yml and customize.'"
-
-        
-
-# regular plugins 
-plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
-plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
-plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
-plugin 'open_id_authentication', :git => 'git://github.com/rails/open_id_authentication.git'
-plugin 'exception_notifier', :git => 'git://github.com/rails/exception_notification.git'
-
-
-# gems  
-gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-gem 'ruby-openid', :lib => 'openid', :source => 'http://gems.github.com'  
-gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com'       
-rake "gems:install", :sudo => true
- 
-# gems  
-rakefile "bootstrap.rake", <<CODE
-  namespace :app do
-    task :bootstrap do
-    end
-    
-    task :seed do
-    end
-  end
-CODE
- 
-generate("authenticated", "user session")
-generate("rspec")
