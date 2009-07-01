4e755e3f63dbaea5ebd9bf8375669184e96ebe83
Aitor García <aitor.garcia@gmail.com>
2009-07-01 12:39:47 +0200

    New template for initialize git and set a few default plugins

--- git_and_rspec.rb
+++ git_and_rspec.rb
@@ -32,3 +32,33 @@ run "cp config/database.yml config/database.yml.sample"
 git :add => "."
  
 git :commit => "-a -m 'Setting up a new rails app. Copy config/database.yml.sample to config/database.yml and customize.'"
+
+        
+
+# regular plugins 
+plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
+plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
+plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
+plugin 'open_id_authentication', :git => 'git://github.com/rails/open_id_authentication.git'
+plugin 'exception_notifier', :git => 'git://github.com/rails/exception_notification.git'
+
+
+# gems  
+gem 'mislav-will_paginate'
+gem 'ruby-openid'
+gem 'rubyist-aasm'      
+rake "gems:install", :sudo => true
+ 
+# gems  
+rakefile "bootstrap.rake", <<CODE
+  namespace :app do
+    task :bootstrap do
+    end
+    
+    task :seed do
+    end
+  end
+CODE
+ 
+generate("authenticated", "user session")
+generate("rspec")
